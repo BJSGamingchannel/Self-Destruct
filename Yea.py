1d7646fb8473c36bcf17f7003f248d8a4d010808
BJSGamingchannel <beaujacksaunders@gmail.com>
2016-03-18 20:46:48 +0000

    Update Yea.py

--- Yea.py
+++ Yea.py
@@ -1,1 +1,11 @@
-Print("test")
+#Welcome to Self-Destruct
+#Author: BJSGamingchannel
+#Programming Language: Python
+
+#Start of program.
+Import TKinter as Tk
+Import os as OS
+Import random as rand
+Import math as M
+
+def Main():
