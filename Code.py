7465a551fb3842f07e2336495f16c538484f567d
BJSGamingchannel <beaujacksaunders@gmail.com>
2016-03-18 21:09:14 +0000

    Update and rename Yea.py to Code.py

--- Code.py
+++ Code.py
@@ -9,9 +9,14 @@ import random as rand
 import math as M
 from Tk import *
 
+font1 = Font("Times New Roman", size = £
+
 def MasterFunction():
+
+
 
 MasterWindow = Tk()
 Can1 = Canvas(master, width=500, height=500)
 Can1.pack()
 TitleMain = Label(MasterWindow, text = "Self Destruct")
+TittleMain.pack()
