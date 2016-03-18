d8942f37e1c6981ac358a8c4118bb1105984ca6a
BJSGamingchannel <beaujacksaunders@gmail.com>
2016-03-18 21:04:15 +0000

    Update Yea.py

--- Yea.py
+++ Yea.py
@@ -7,5 +7,11 @@ import TKinter as Tk
 import os as OS
 import random as rand
 import math as M
+from Tk import *
 
-def Main():
+def MasterFunction():
+
+MasterWindow = Tk()
+Can1 = Canvas(master, width=500, height=500)
+Can1.pack()
+TitleMain = Label(MasterWindow, text = "Self Destruct")
