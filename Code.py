522e414209899d6bac5f546104ae5485a73a8613
BJSGamingchannel <beaujacksaunders@gmail.com>
2016-03-18 21:50:24 +0000

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,18 +3,23 @@
 #Programming Language: Python
 
 #Start of program.
-import TKinter as Tk
+
+import tkinter as Tk
+from tkinter import *
 import os as OS
 import random as rand
 import math as M
-from Tk import *
 
-font1 = Font(family = "Times New Roman", size = "20", weight= "bold")
+font1 = ("Times New Roman", "20", "bold")
 
 def MasterFunction():
   
-MasterWindow = Tk()
-Can1 = Canvas(master, width=500, height=500)
-Can1.pack()
-TitleMain = Label(MasterWindow, text = "Self Destruct", font = font1, fg = blue)
-TittleMain.pack()
+    MasterWindow = Tk()
+    MasterWindow.geometry("1350x740")
+    MasterWindow.title("Self Destruct")
+    #MasterWindow.wm_iconbitmap ('C:\Users\bjsga_000\Documents\GitHub\Self-Destruct-master\Bomb.ico')#This don't work yet.
+    Can1 = Canvas(MasterWindow, width=500, height=500)
+    TitleMain = Label(MasterWindow, text = "Self Destruct", font = font1, fg = "blue")
+    TitleMain.pack()
+
+MasterFunction()
