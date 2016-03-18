11c32e9a026aea3b7aa89acf313a53afc776d9f9
BJSGamingchannel <beaujacksaunders@gmail.com>
2016-03-18 21:10:26 +0000

    Update Code.py

--- Code.py
+++ Code.py
@@ -9,7 +9,7 @@ import random as rand
 import math as M
 from Tk import *
 
-font1 = Font("Times New Roman", size = £
+font1 = Font(family = "Times New Roman", size = "20", weight= "bold")
 
 def MasterFunction():
 
@@ -18,5 +18,5 @@ def MasterFunction():
 MasterWindow = Tk()
 Can1 = Canvas(master, width=500, height=500)
 Can1.pack()
-TitleMain = Label(MasterWindow, text = "Self Destruct")
+TitleMain = Label(MasterWindow, text = "Self Destruct", font = font1, fg = blue)
 TittleMain.pack()
